36af4de1b8b82c124a0d303ff99c9e3838258408
Nico Götze <goetze.nico@hotmail.de>
2024-08-26 18:39:49 +0200

    Datenbankmodell für Country erstellt

--- predictions/models.py
+++ predictions/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Country(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=100, verbose_name='Country Name', db_index=True)
+    name_de = models.CharField(max_length=100, verbose_name='Country Name (German)', db_index=True)
+    iso = models.CharField(max_length=2, verbose_name='ISO Code', help_text='ISO 3166-1 alpha-2 code')
+    iso_number = models.CharField(max_length=3, verbose_name='ISO Number', help_text='ISO 3166-1 numeric code')
+    slug = models.SlugField(max_length=100, verbose_name='Slug', db_index=True, unique=True)
+    
+    class Meta:
+        verbose_name = 'Country'
+        verbose_name_plural = 'Countries'
+        ordering = ['name']
+        
+    def __str__(self):
+        return self.name
